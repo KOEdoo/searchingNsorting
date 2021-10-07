d09eb8773cfd334a12d8eb62fcdd00194b4303c1
KOEdoo <edookofi@gmail.com>
2021-10-07 19:35:34 +0000

    included other sorting algorithms

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,5 +1,7 @@
+import java.util.*;
 public class Sorting {
     static void sort (int algorithmID, int[] arr){
+        int n = arr.length;
         switch (algorithmID){
             case 1:
                 selectionSort(arr);
@@ -10,17 +12,18 @@ public class Sorting {
             case 3:
                 shellShort(arr);
                 break;
+            case 4:              
+                quickSort(arr, 0, n-1);
+                break;
+            case 5:              
+                mergeSort(arr, 0, n-1);
+                break;
+            case 6:              
+                radixSort(arr, n);
+                break;
         }
     }
-// algoritm ID = 1 => selection sort
-// algoritm ID = 2 => insertion sort
-// algoritm ID = 3 => shell sort
-// algoritm ID = 4 => quick sort
-// algoritm ID = 5 => merge sort
-// algoritm ID = 6 => radix sort
-
-//create sort algorithm functions for each 
-//include their switch cases in the sort function following numbering of the above comment 
+ 
     static void selectionSort(int[] arr){
         int n = arr.length;      
         for (int i = 0; i < n-1; i++)
@@ -60,6 +63,119 @@ public class Sorting {
                     arr[j] = arr[j - gap];
                 arr[j] = temp;
             }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static int partition(int[] arr, int low, int high){
+        int pivot = arr[high]; 
+        int i = (low - 1); 
+    
+        for(int j = low; j <= high - 1; j++){
+            if (arr[j] < pivot) {
+                i++; 
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+}
+    static void quickSort(int[] arr, int low, int high){
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+    static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; 
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+        for (i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+        for (i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+    static void radixSort(int arr[], int n)
+    {
+        int m = getMax(arr, n);
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+
+    static void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    static void mergeSort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
+  
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+  
+            // Merge the sorted halves
+            merge(arr, l, m, r);
         }
     }
 }
